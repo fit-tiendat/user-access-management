c38bd488edd213251bcdfd4b907fa0891ccbf3d8
Tien Dat <22704321.dat@student.iuh.edu.vn>
2026-07-28 14:45:19 +0700

    perf(database): mark read services read-only

--- user-service/src/main/java/com/r2s/user/service/ProfileServiceImpl.java
+++ user-service/src/main/java/com/r2s/user/service/ProfileServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProfileServiceImpl implements ProfileService {
 
     private final ProfileRepository repo;
